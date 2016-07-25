c50b4f222af201a15f478e75af5db04b0ee98c8c
Antonio Vidal <antoniovidal100@hotmail.com>
2016-07-25 18:29:18 -0500

    3.4 Refactoring       Unified format of static pages tests (pages titles)

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -15,6 +15,6 @@ class StaticPagesControllerTest < ActionController::TestCase
   test	"should	get	about"	do
     get	:about
     assert_response	:success
-    assert_select	"title",	"About |	Ruby	on	Rails	Tutorial	Sample	App"
+    assert_select	"title",	"About	|	Ruby	on	Rails	Tutorial	Sample	App"
   end
 end
