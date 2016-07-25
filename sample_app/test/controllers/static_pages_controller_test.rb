2b915b6dfccd0433f2b1943e9aff34fa5ae9b5df
Antonio Vidal <antoniovidal100@hotmail.com>
2016-07-25 18:14:38 -0500

    3.4 Refactoring       Added quick (redundant) solution to views (static pages) titles.       Corrected spell mistakes at tests.

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -13,8 +13,8 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select	"title",	"Help	|	Ruby	on	Rails	Tutorial	Sample	App"
   end
   test	"should	get	about"	do
-    assert_select	"title",	"About	|	Ruby	on	Rails	Tutorial	Sample	App"
     get	:about
     assert_response	:success
+    assert_select	"title",	"About |	Ruby	on	Rails	Tutorial	Sample	App"
   end
 end
